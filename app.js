6788b7bdbf26ef6d553845a4e55a1cd8aa671638
Lis Pacheco <lmpacheco@uce.edu.ec>
2025-07-05 20:13:33 -0500

    chore: enable CORS in adoption-request-service

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const routes = require('./routes/adoptionRequest.routes');
 const swaggerUi = require('swagger-ui-express');
@@ -7,9 +8,9 @@ const YAML = require('yamljs');
 
 const swaggerDocument = YAML.load('./docs/swagger.yaml');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/adoptionRequest-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/', routes);
 
 module.exports = app;
-
